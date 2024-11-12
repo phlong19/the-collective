aed4ab057f5ff7724da2c9b59498633d0d4e94c8
hagen.pham <hagen.pham@avepoint.com>
2024-11-12 15:51:34 +0700

    Merge branch 'master' of https://github.com/phlong19/the-collective

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // libs
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // layouts
@@ -8,6 +9,10 @@ import AppLayout from "./components/layout/AppLayout";
 // ui
 import NotFound404 from "./components/ui/NotFound404";
 import Event from "./pages/Event";
+// ui
+import ScrollToTop from "./components/ui/ScrollToTop";
+import Articles from "./pages/Articles";
+import Events from "./pages/Events";
 // pages
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
@@ -18,11 +23,12 @@ function App() {
   return (
     <QueryClientProvider client={client}>
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route element={<AppLayout />}>
             <Route path="/" element={<Home />} />
-            <Route path="events" />
-            <Route path="articles" />
+            <Route path="events" element={<Events />} />
+            <Route path="articles" element={<Articles />} />
             <Route path="event/:slug" element={<Event />} />
             <Route path="article/:slug" />
             <Route path="global-search" />
@@ -51,6 +57,7 @@ function App() {
           <Route path="terms-of-use" />
           <Route path="FAQs" />
 
+          <Route path="maintenance" />
           <Route path="maintenance" />
 
           <Route path="*" element={<NotFound404 />} />
